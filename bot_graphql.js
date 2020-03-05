951852f05a6e6b6af17ed6da5fbe7e25a4ad73e7
Anna <nuro4ka2011@yandex.ru>
2020-03-05 20:36:08 +0300

    added graphql version

---
+++ bot_graphql.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const puppeteer = require('puppeteer');
+let name_index = 0
+
+async function main() {
+  const browser = await puppeteer.launch({
+        headless: false,
+        args: ['--no-sandbox']
+    });
+  const page = await browser.newPage();
+  const fs = require("fs");
+
+  //await page.setRequestInterception(true);
+
+
+  page.on('request', request => {
+    if (request.method() == 'POST') {
+      let name = `${name_index}.txt`
+      //console.log(request.method() + ' '+ request.url());
+      let data = request.method() + ' '+ request.url();
+
+      let current_headers = request.headers();
+      for (let key in current_headers) {
+        //console.log(key, current_headers[key]);
+        data += '\n' + key + ' ' + current_headers[key];
+      }
+      //console.log(request.postData());
+      data += '\n\n' + request.postData();
+
+      console.log(data);
+      if (request.url().indexOf('graphql') != -1) {
+       fs.writeFileSync(name, data);
+      }
+      name_index += 1;
+    }
+  });
+  ;
+
+  await page.goto('https://www.coursera.org/');
+
+  //console.log(cookies);
+
+  function dumpFrameTree(frame, indent) {
+    console.log(indent + frame.url());
+    for (let child of frame.childFrames())
+      dumpFrameTree(child, indent + '  ');
+
+  }
+}
+
+// Start the script
+main();
